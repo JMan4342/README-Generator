996cfdcc9566563a7b881b089cc3f7ad0ae9c7db
Joseph Manning <jgmanning3@yahoo.com>
2021-06-20 15:47:37 -0700

    Create prompt questions

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # README-Generator
+![GitHub license]
+
+## Description
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usagae)
+
+* [License](#license)
+
+* [Contributing](#contribution)
+
+* [Test](#tests)
+
+* [Questions](#questions)
+
+## Intallation
+
+To install necessary dependencies, run the following command:
+
+...
+
+...
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at (email address). You can find more of my work at [GitHub username](GitHub link).
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+fs.readFile("generateMarkdowns.js");
 
 // TODO: Include packages needed for this application
 
@@ -15,9 +16,64 @@ function init() {}
 // Function call to initialize app
 init();
 
+// const questions = [
+//     {
+
+//     },
+// ]
+
 inquirer
-.prompt ([
+  .prompt([
     {
-        type: "input",
-    }
-])
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your project's name?",
+      name: "project",
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project.",
+      name: "description",
+    },
+    {
+      type: "list",
+      message: "What kind of license shoudl your project have?",
+      name: "license",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+      type: "input",
+      message: "What command should be run to install dependencies?",
+      name: "command",
+    },
+    {
+      type: "input",
+      message: "What does the user need to know about using the repo?",
+      name: "usingRepo",
+    },
+    {
+      type: "input",
+      message:
+        "What does the user need to know about contributing to the repo?",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ])
+  .then(function (user) {
+    fs.writeFile("README.md", generateMarkdown(user), function (err) {
+      console.log("User:", user);
+    });
+  });
